a169db158d55a24a5d014560c030f42cc5e53b0c
tomcarney <thomas.carney@griffithuni.edu.au>
2021-03-02 17:11:40 +1000

    problem 1.1 complete

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -15,12 +15,19 @@ struct line{
 bool isCounterClockwise(point a, point b, point c);
 
 int main(){
-    point polygon[Nmax] = {{1,2}, {2, 3}, {3, 4}};
+    point polygon[Nmax] = {{0,0}, {0, 1}, {-1, 0}};
     isCounterClockwise(polygon[0], polygon[1], polygon[2]);
 
+    return 0;
 }
 
+// returns true if 3 points form a counter clockwise angle.
 bool isCounterClockwise(point a, point b, point c){
-    std::cout << a.x << std::endl;
-    return true;
+    if(a.x*b.y - a.y*b.x + a.y*c.x - a.x*c.y + b.x*c.y - c.x*b.y > 0){
+        std::cout << "Counter Clockwise." << std::endl;
+        return true;
+    } else {
+        std::cout << "Clockwise." << std::endl;
+        return false;
+    }
 }
